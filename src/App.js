30deeaa9d61949a9b8dff33ac7aaf584e4919a73
Krishna <callmekrsh@gmail.com>
2024-03-13 19:14:26 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Home from "../src/Codex/Components/Home";
+import About from "../src/Codex/Components/About";
+import Footer from "../src/Codex/Components/Footer";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About/>
+      <Footer />
+     
+    </div>
+  );
+}
+
+export default App;
